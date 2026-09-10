15db83ee81214bc439f6c206f396b5245fb01b4c
sbaandaya <sbaandaya@brc.pshs.edu.ph>
2026-09-10 12:42:43 +0800

    Update classRelationship.py

--- Activities/classRelationship.py
+++ Activities/classRelationship.py
@@ -1,1 +1,86 @@
+class Restaurant:
+    def __init__(self, serving, closing, checkingstock, stock, restaurantname, amountofstock, openorclose, location):
+        self.serving = serving
+        self.closing = False
+        self.__checkingstock = checkingstock
+        self.__updatingstock = stock
+        self.restaurantname = restaurantname
+        self.__amountofstock = amountofstock
+        self.openorclose = openorclose
+        self.location = location
+    def display_restaurantname(self):
+        print("Restaurant Name:", self.restaurantname)
+    def __display_amountofstock(self):
+        print("Amount of Stock:", self.__amountofstock)
+    def display_openorclose(self):
+        print("Open or Close:", self.openorclose)
+    def display_location(self):
+        print("Restaurant Location:", self.location)
+        self.__display_amountofstock()
+        self.display_openorclose()
+        self.display_restaurantname()
+    def display_closing(self, value):
+        self.closing = value
+    def __display_checkingstock(self):
+        print("Current amount of stock available:", self.__checkingstock)
+    def __add_stock(self, number):
+        self.__updatingstock += number
+        print("New amount of stock available:", self.__updatingstock)
+    def __minus_stock(self, number):
+        self.__updatingstock -= number
+        print("New amount of stock available:", self.__updatingstock)
+    def __change_stock(self, number, operation):
+        if operation == "add":
+            self.__add_stock(number)
+        elif operation == "minus":
+            self.__minus_stock(number)
+    def display_serving(self):
+        print("Customer is", self.serving)
+        print("The store is now", self.closing)
+        self.__display_checkingstock()
+        print("New amount of stock available:", self.__updatingstock)
+
+restaurant1 = Restaurant("Served", "False", 50, 50, "Bread Talker", 50, "Open", "SM Legazpi")
+restaurant2 = Restaurant("Served", "True", 60, 60, "Bread Hater", 60, "Close", "SM Naga")
+restaurant1.display_location()
+restaurant1.display_serving()
+restaurant1.display_closing(True)
+restaurant1._Restaurant__change_stock(10, "add")
+print("")
+restaurant2.display_location()
+restaurant2.display_serving()
+restaurant2._Restaurant__change_stock(15, "minus")
+
+
+
+class Staff:
+  def __init__(self, Name, Age, Profession, Rating, Serving, Closing, Cleaning, Stocking):
+    self.Name = Name
+    self.Age = Age
+    self.Profession = Profession
+    self.Rating = Rating
+    self.Serving = False 
+    self.Closing = False
+    self.Cleaning = Cleaning
+    self.Stocking = Stocking
+    self.related_objects = []
+def display_Name(self):
+   print("Staff Name:", self.Name)
+def display_Age(self):
+  print("Staff Age", self.Age)
+def display_Profession(self):
+  print("Staff Profession", self.Profession)
+def display_Rating(self):
+  print("Staff Rating is", self.Rating)
+def display_Serving(self, value):
+  self.Serving = value
+def display_Closing(self, value):
+  self.Closing = value
+def display_Cleaning(self):
+  print("The staff already ", self.Cleaning)
+def display_Stocking(self):
+  print("The staff stocked ", self.Stocking)
+
+self.related_objects = []
+
 
